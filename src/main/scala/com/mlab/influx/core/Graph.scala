22a9e777d82cb2050e7414ffd64bb0491577dbe5
Noah Golmant <noah.golmant@gmail.com>
2017-03-19 16:22:57 -0700

    remove graphs

--- src/main/scala/com/mlab/influx/core/Graph.scala
+++
@@ -1,9 +0,0 @@
-package com.mlab.influx.core
-
-/**
-  * Created by suneelbelkhale1 on 3/14/17.
-  */
-trait Graph[TermIn, TermOut] {
-  var outNode: ConnectedNode[TermIn, TermOut]
-  private[core] def connect(in: Graph[Any, TermIn]): Graph[TermIn, TermOut] = outNode.connect(in)
-}
--- src/main/scala/com/mlab/influx/core/Node.scala
+++ src/main/scala/com/mlab/influx/core/Node.scala
@@ -5,20 +5,34 @@ import org.apache.spark.streaming.dstream.DStream
 /**
   * Created by suneelbelkhale1 on 3/14/17.
   */
-class Node[A,B](applyFunc: Function[A,B]) extends Graph[A,B]{
+abstract class Node[A, B] {
+  private var out: Option[DStream[B]] = None
+  private var prevNode: Option[Node[Any, A]] = None
 
-  var out: DStream[B] = null
-  var prevNode: Node[Any,A] = null
-  override var outNode: ConnectedNode[A, B] = new ConnectedNode[A,B](this)
-
-  override private[core] def connect(in: Graph[Any, A]): ConnectedNode[A,B] = {
-      this.out = apply(in.outNode.out.asInstanceOf[DStream[A]])
-      prevNode = in.outNode.asInstanceOf[Node[Any,A]]
-      return new ConnectedNode[A,B](this)
+  private[core] def connect(in: Node[Any, A]): Node[A,B] = {
+      this.out = in.out match {
+        case Some(stream) => Some(apply(stream))
+        case None => throw new UnsupportedOperationException("Must connect to connected node")
+      }
+      this.prevNode = Some(in.asInstanceOf[Node[Any, A]])
+      this
   }
 
-  def apply(in: A) : B = applyFunc(in)
-
+  def apply(in: A) : B
   def apply(inStream: DStream[A]) : DStream[B] = inStream.map(apply)
+}
 
+object Node {
+  /**
+    * This constructor takes a function and returns a Node that maps it over the input stream
+    *
+    * @param f The function to apply to every item in the RDD being transformed
+    * @tparam A input type of the node
+    * @tparam B output type of the node
+    * @return Node that applies the given function to all items in the RDD
+    */
+  def apply[A, B](f: A => B): Node[A, B] = new Node[A, B] {
+    override def apply(in: DStream[A]): DStream[B] = in.map(f)
+    override def apply(in: A): B = f(in)
+  }
 }
