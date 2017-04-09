e90eb40a6ab2ab20f73803561aec58f74dfdc323
Noah Golmant <noah.golmant@gmail.com>
2017-04-09 15:35:41 -0700

    add mutable accumulator

--- src/main/scala/com/mlab/influx/core/MutableNode.scala
+++ src/main/scala/com/mlab/influx/core/MutableNode.scala
@@ -1,12 +1,31 @@
 package com.mlab.influx.core
 
 import org.apache.spark.util.AccumulatorV2
+
+
+class Accumulator[C](initialState: C) extends AccumulatorV2[C, C] {
+  private var state: C = initialState
+
+  def reset(): Unit = { state = initialState}
+  def add(in: C): Unit = {state = in }
+
+  override def isZero: Boolean = false
+
+  override def copy(): AccumulatorV2[C, C] = new Accumulator(state)
+
+  override def merge(other: AccumulatorV2[C, C]): Unit = { state = other.value }
+
+  override def value: C = state
+}
+
 /**
   * Created by ravi on 4/4/17.
   */
 abstract class MutableNode[A, B, C] extends Node[A, B] {
-  val state: AccumulatorV2[C, C]
-  def update(in: C) : Unit
+  val initialState: C
+
+  val state: Accumulator[C] = new Accumulator(initialState)
+  def update(in: C) : Unit = { state.add(in) }
 }
 
 object MutableNode {
