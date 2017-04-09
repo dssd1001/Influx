d473e685ba020a2cd36c5cfa8e521c9a61724a09
suneelbelkhale <neelus.genius@gmail.com>
2017-04-09 16:23:17 -0700

    some component stuff

--- src/main/scala/com/mlab/influx/core/Component.scala
+++ src/main/scala/com/mlab/influx/core/Component.scala
@@ -5,33 +5,26 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by noahg on 4/4/2017.
   */
-class Component extends Graph {
-  private val nodes: Seq[Node[Any, Any]] = ArrayBuffer.empty[Node[Any, Any]]
+class Component(input: Operator, output: Operator) {
+  private val nodes: Seq[Operator] = ArrayBuffer.empty[Operator]
   private val edges: Seq[Edge] = ArrayBuffer.empty[Edge]
-
-  val input: Node[Any, Any]
-  val output: Node[Any, Any]
 
   private val defaultInput = Some(input)
   private val defaultOutput = Some(output)
 
-  def hook(from: Node[Any, Any], to: Node[Any, Any]): Unit = {
+  def hook(from: Operator, to: Operator): Unit = {
     /** CODE TO GET TYPES **/ 
     val fromNode = from.asInstanceOf[Node[from.IN,from.OUT]] 
     val toNode = to.asInstanceOf[Node[to.IN,to.OUT]]  
 
-    if (!nodes.contains(from)) nodes += fromNode
-    if (!nodes.contains(to)) nodes += toNode
-    edges += Edge(fromNode, toNode)
+    if (!nodes.contains(from)) nodes :+ from
+    if (!nodes.contains(to)) nodes :+ to
+    edges :+ Edge(from, to)
   }
 
-  def connect(leftNode: Node[Any, Any], rightNode: Node[Any, Any], mutableNode: MutableNode[A, B, C]): Unit = {
-
-  }
-
-  def addNode(node: Node[Any, Any]): Unit = {
+  def addNode(node: Operator): Unit = {
     if (!nodes.contains(node))
-      nodes = nodes :+ node
+      nodes :+ node
   }
 
 }
