1d187337da518a83b34d506c8914a88bc32232a8
Noah Golmant <noah.golmant@gmail.com>
2017-04-11 20:38:02 -0700

    fix args on component

--- src/main/scala/com/mlab/influx/core/Component.scala
+++ src/main/scala/com/mlab/influx/core/Component.scala
@@ -5,12 +5,12 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by noahg on 4/4/2017.
   */
-class Component(input: Operator, output: Operator) {
+abstract class Component {
   private val nodes: Seq[Operator] = ArrayBuffer.empty[Operator]
   private val edges: Seq[Edge] = ArrayBuffer.empty[Edge]
 
-  private val defaultInput = Some(input)
-  private val defaultOutput = Some(output)
+  val input: Operator
+  val output: Operator
 
   def hook(from: Operator, to: Operator): Unit = {
     /** CODE TO GET TYPES **/ 
