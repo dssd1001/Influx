dd193ac0cd8bc470632e93d4d123742a2008c5ae
Ravi Pandya <rpandya922@berkeley.edu>
2017-04-16 00:09:48 -0700

    initial implementation for Logistic Regression

--- src/main/scala/com/mlab/influx/ml/LogisticRegressionGraph.scala
+++ src/main/scala/com/mlab/influx/ml/LogisticRegressionGraph.scala
@@ -2,14 +2,37 @@ package com.mlab.influx.ml
 
 import org.apache.spark.streaming.dstream.DStream
 import breeze.linalg._
+import breeze.math._
 import breeze.numerics._
+import com.mlab.influx.core.Node
+import org.apache.spark.rdd.RDD
 
 /**
   * Created by ravi on 4/11/17.
   */
-class LogisticRegressionGraph[A <: Vector, B <: Int, M <: Vector](trainStream: DStream[(A, B)], queryStream: DStream[A], size: Int)
-  extends MLModelGraph[A, B, M](trainStream, queryStream){
+class LogisticRegressionGraph[A <: Vector](trainStream: DStream[(A, Int)], queryStream: DStream[A], size: Int, learningRate: Double)
+  extends MLModelGraph[A, Int, A](trainStream, queryStream){
 
-  val initialModel = Vector.fill(size)(0)
-  override def learn(trainData: A, trainLabels: B, oldModel: M)
+  val initialModel = DenseVector.zeros[Double](size)
+  val input = Node(x => x)
+  val output = Node(x => x)
+
+  override def learn(trainData: A, trainLabels: Int, oldModel: A): A = {
+    val z = trainLabels - sigmoid(oldModel * trainData)
+    oldModel - (learningRate * z) * trainData
+  }
+  def learn(trainData: RDD[A], trainLabels: RDD[Int], oldModel: A): A = {
+    val data = DenseVector(trainData.collect())
+    val labels = DenseVector(trainLabels.collect())
+    val z = labels - sigmoid(data.t * oldModel)
+    oldModel - (data.t * (learningRate * z))
+  }
+  override def predict(data: A, model: A): Int = {
+    if(sigmoid(data * model).asInstanceOf[Double] < 0.5) -1 else 1
+  }
+  def predict(data: RDD[A], model: A): RDD[Int] = {
+    data.map(
+      x => if(sigmoid(x * model).asInstanceOf[Double] < 0.5) -1 else 1
+    )
+  }
 }
