bffdbda321e4b2772e4f04781c494fcae3977e20
Ravi Pandya <rpandya922@berkeley.edu>
2017-04-20 20:56:31 -0700

    started component tests

--- src/main/test/com/mlab/influx/core/Component.scala
+++
@@ -1,8 +0,0 @@
-package com.mlab.influx.core
-
-/**
-  * Created by noahg on 4/20/2017.
-  */
-class Component {
-
-}
---
+++ src/main/test/com/mlab/influx/core/ComponentTests.scala
@@ -0,0 +1,34 @@
+package com.mlab.influx.core
+
+import org.apache.spark.SparkContext
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+/**
+  * Created by noahg on 4/20/2017.
+  */
+class ComponentTests extends FunSuite with BeforeAndAfter {
+  var sc: SparkContext = _
+  var math : MathComponent = _
+
+  before {
+    val conf = new SparkConf().setAppName("Node tests").setMaster("localhost")
+    sc = new SparkContext(conf)
+  }
+
+  test("Component does math") {
+    assert(math.extractFunction.apply(1) == 3)
+  }
+
+  after {
+    sc.stop()
+  }
+}
+
+class MathComponent extends Component {
+  val multNode : Node(x => x * 2)
+  val addNode : Node(x => x + 1)
+
+  this.hook(multNode, addNode)
+  this.setDefaultInput(multNode)
+  this.setDefaultOutput(addNode)
+}
